e9938e2a70f8877c4f71b85b88da40a521afa1da
MrSkorohod <mr.vl.skorohod@gmail.com>
2024-04-04 10:56:51 +0300

    Added theme and moved games components in another folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Game from "./components/game/game";
+import Game from '@/components/game/game';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 export default function Home() {
   return (
-    <main>
-      <Game />
-    </main>
+    <Container maxWidth='lg'>
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Game />
+      </Box>
+    </Container>
   );
 }
